58bfbc950177a453fed3536ea308bbd1beb8743a
cheng-hsin <s950091qq@gmail.com>
2022-09-12 18:05:53 +0800

    Assignment4 is done

--- week-1/Assignment-4/Assignment-4.js
+++ week-1/Assignment-4/Assignment-4.js
@@ -1,23 +1,32 @@
-
 function avg(data) {
-    // your code here
+  // your code here
+  var sum = 0;
+  for (var i = 0; i < data.size; i++) {
+    sum += data.products[i].price;
+    console.log(sum);
+    if (i == data.size - 1) {
+      return sum / data.size;
     }
-    console.log(
-    avg({
+  }
+}
+
+console.log(
+  avg({
     size: 3,
     products: [
-    {
-    name: 'Product 1',
-    price: 100,
-    },
-    {
-    name: 'Product 2',
-    price: 700,
-    },
-    {
-    name: 'Product 3',
-    price: 250,
-    },
+      {
+        name: 'Product 1',
+        price: 100,
+      },
+      {
+        name: 'Product 2',
+        price: 700,
+      },
+      {
+        name: 'Product 3',
+        price: 250,
+      },
     ],
-    })
-    ); // should print the average price of all products
+  })
+);
+// should print the average price of all products
