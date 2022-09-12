def8241712aa287b9b3a5c3f9c3d32d1f3275c5b
cheng-hsin <s950091qq@gmail.com>
2022-09-12 16:51:51 +0800

    update assignment4

--- week-1/Assignment-4/Assignment-4.js
+++ week-1/Assignment-4/Assignment-4.js
@@ -1,1 +1,23 @@
 
+function avg(data) {
+    // your code here
+    }
+    console.log(
+    avg({
+    size: 3,
+    products: [
+    {
+    name: 'Product 1',
+    price: 100,
+    },
+    {
+    name: 'Product 2',
+    price: 700,
+    },
+    {
+    name: 'Product 3',
+    price: 250,
+    },
+    ],
+    })
+    ); // should print the average price of all products
