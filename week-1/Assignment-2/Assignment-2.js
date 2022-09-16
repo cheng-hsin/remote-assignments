1113d0207dd47cc240fec9f2da1532f985217c69
cheng-hsin <s950091qq@gmail.com>
2022-09-16 20:52:34 +0800

    optimize A2~

--- week-1/Assignment-2/Assignment-2.js
+++ week-1/Assignment-2/Assignment-2.js
@@ -16,38 +16,10 @@ function countAandB(input) {
 
 function toNumber(input) {
   // your code here
-  //input2 = ['e', 'd', 'c', 'd', 'e'];
-  arr = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  ];
   newArr = [];
   for (i = 0; i < input.length; i++) {
-    newArr.push(arr.indexOf(`${input[i]}`) + 1);
+    // newArr.push(arr.indexOf(`${input[i]}`) + 1);
+    newArr.push(`${input[i]}`.charCodeAt(0) - 96);
   }
   return newArr;
 }
